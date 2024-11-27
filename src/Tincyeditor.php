3050751c044bded1513d6a4436f112f43b3a0f10
mohamed7sameer <mohammed7sameer2617@gmail.com>
2024-11-28 00:20:10 +0200

    first commit

---
+++ src/Tincyeditor.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mohammed2617\Tincyeditor;
+
+use Laravel\Nova\Fields\Field;
+
+class Tincyeditor extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'tincyeditor';
+}
